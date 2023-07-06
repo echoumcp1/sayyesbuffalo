945ccb9f2ae186c39c0f7c58ec28eb48c198015f
Kevin Zhou <akvnzh@gmail.com>
2023-07-06 15:42:11 -0400

    email

--- email_buf.py
+++ email_buf.py
@@ -1,19 +1,22 @@
 import smtplib
 from email.mime.text import MIMEText
+from flask import Flask, jsonify, request
 
 subject = "Application Follow-up"
 sender = "SayYesTeam2@gmail.com"
 password = "wxvajsarlasdavql"
 
-# PASS 
-body = "This is the body of the text message"
-recipients = ["akvnzh@gmail.com"]
+app = Flask(__name__)
 
-def send_email(subject, body, sender, recipients, password):
-    msg = MIMEText(body)
+@app.route('/send_email', methods=['POST'])
+def send_email():
+    # from json: email, company
+    data = request.get_json()  
+    
+    msg = MIMEText("Please update us about your application to " + data["company"] + "!")
     msg['Subject'] = subject
     msg['From'] = sender
-    msg['To'] = ', '.join(recipients)
+    msg['To'] = [data["email"]]
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
        smtp_server.login(sender, password)
        smtp_server.sendmail(sender, recipients, msg.as_string())
@@ -21,4 +24,4 @@ def send_email(subject, body, sender, recipients, password):
 
 
 if __name__ == '__main__':
-    send_email(subject, body, sender, recipients, password)
+    app.run()
