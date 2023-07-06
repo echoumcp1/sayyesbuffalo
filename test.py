2ccc46429b37327d090cbb57028f80bc88987ee0
echoumcp1 <101858583+echoumcp1@users.noreply.github.com>
2023-07-06 14:29:22 -0400

    added flask stuff

--- test.py
+++ test.py
@@ -11,6 +11,19 @@ connection = f'DRIVER={driver};SERVER=tcp:{server};PORT=1433;DATABASE={database}
 conn = pyodbc.connect(connection)
 cur = conn.cursor()
 
+app = Flask(__name__)
 
+@app.route('/insert_user', methods=['POST'])
+def insert_user():
+    data = request.get_json()  
+    first, last, email = data['Firstname'], data['Lastname'], data['Email']
+
+    cur.execute("INSERT INTO Users VALUES (?,?,?);", (first, last, email))
+    conn.commit()
+
+    return jsonify({'msg':'data insertion success'})
+
+if __name__ == '__main__':
+    app.run()
     
         
