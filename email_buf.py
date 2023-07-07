f65364940f9e7e1d15509fbbc633bb4490d37de7
echoumcp1 <101858583+echoumcp1@users.noreply.github.com>
2023-07-07 09:41:41 -0400

    a

--- email_buf.py
+++ email_buf.py
@@ -10,14 +10,16 @@ app = Flask(__name__)
 
 @app.route('/send_email', methods=['POST'])
 def send_email():
-    msg = MIMEText("Please update us about your application to " + request.args.get("company") + "!")
+    data = request.get_json()
+    print(data)
+    msg = MIMEText("Please update us about your application to " + data.get("company") + "!")
     
     msg['Subject'] = subject
     msg['From'] = sender
-    msg['To'] = request.args.get("email")
+    msg['To'] = data.get("email")
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
        smtp_server.login(sender, password)
-       smtp_server.sendmail(sender, request.args.get("email"), msg.as_string())
+       smtp_server.sendmail(sender, data.get("email"), msg.as_string())
     return jsonify({'msg':'email sent'})
 
 
--- test.py
+++ test.py
@@ -2,12 +2,18 @@ import pyodbc
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import datetime
+import smtplib
+from email.mime.text import MIMEText
 
 server = 'sayyesbuffalo1.database.windows.net'
 database = 'sayyesbuffalo1'
 username = 'echou1'
-password = 'Bonkers123'   
+password = ""
 driver= '{ODBC Driver 17 for SQL Server}'
+
+subject = "Application Follow-up"
+sender = "SayYesTeam2@gmail.com"
+passwd = ""
 
 connection = f'DRIVER={driver};SERVER=tcp:{server};PORT=1433;DATABASE={database};UID={username};PWD={password}'
 conn = pyodbc.connect(connection)
@@ -35,15 +41,36 @@ def insert_user():
 @app.route('/add_app', methods=['POST'])
 def insert_app():
     data = request.get_json()
-    email, company_id = data['Email'], data['ID']
+    email, company_id = data['Email'], data['Job']
 
     cur.execute("INSERT INTO Application VALUES (?, ?, ?, ?);", (email, company_id, datetime.datetime.now(), 0))
     conn.commit()
 
+    cur.execute(f"SELECT Name FROM Companies WHERE ID = {company_id}")
+    company_name = cur.fetchone()[0]
+    send_email(email, company_name)
+
     return jsonify({'msg': 'application insertion success'})
 
+def send_email(receiver, company):
+    msg = MIMEText("Please update us about your application to " + company + "!")
+    
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = receiver
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+       smtp_server.login(sender, passwd)
+       smtp_server.sendmail(sender, receiver, msg.as_string())
+
+
+@app.route('/submit', methods=['POST'])
 def change_status():
-    pass
+    data = request.get_json()
+    email, company_id, status = data['Email'], data['ID'], data['Status']
+    cur.execute(f"UPDATE Applications SET Status={status} WHERE Email = \'{email}\' AND ID = {company_id}")
+    conn.commit()
+
+    return "Update success"
 
 @app.route('/get_companies', methods=['GET'])
 def get_companies():
@@ -62,12 +89,7 @@ def exists_email():
     email = request.args["email"]
     cur.execute(f"SELECT * FROM Users WHERE Email = \'{email}\'")
     data = cur.fetchall()
-    print(data)
-    print(len(data))
-    if len(data):
-        return "Success", 200
-    else:
-        return "Failure", 400
+    return ("Success", 200) if len(data) != 0 else ("Failure", 400)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=3001)
